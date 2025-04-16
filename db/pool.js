4b5dce4cfa954e5b836d3946afb067a427ca5f8a
ImZirix <xxkillerxx.xg@gmail.com>
2025-04-16 16:14:07 +0300

    .

--- db/pool.js
+++ db/pool.js
@@ -3,6 +3,6 @@ const { Pool } = require("pg");
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
   },
 });
